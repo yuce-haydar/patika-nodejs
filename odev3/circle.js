456d4be8eab14a81ebfc7ece7bf7a1a1be5b3afd
HAYDAR <HAYDAR@DESKTOP-7DG9GQL>
2023-06-05 22:09:24 +0300

    module kullanımnı ogrenıyoruz :)

---
+++ odev3/circle.js
@@ -0,0 +1,12 @@
+ function circleArea(r){
+    let alan=r**2*3.14
+  console.log('alanı:',alan)
+}
+ function circleCircumference (r){
+    console.log('yarıcap:',r)
+}
+//!fonksiyonlarımızı actık dısarı actık
+module.exports={
+circleArea,
+circleCircumference
+}
